46c6642b8e51c1a72e02a2855ead346e357fe403
ayxo <a91913@gmail.com>
2020-10-24 23:34:38 +0800

    feat: add meta desc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import Head from 'next/head'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>VAwesome - Explore awesome repositories</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="A new way to view awesome repositories, you can type the github link below to view the list of awesome things."
+        />
       </Head>
       <Component {...pageProps} />
     </>
